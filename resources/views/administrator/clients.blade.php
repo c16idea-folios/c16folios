c165f1bee2ef2fee6c528d4e761bdc3e6eaaebaf
Javier Evans <evansjavier16@gmail.com>
2025-08-20 09:07:50 -0400

    refactor: separar modal de crear cliente en vista parcial, reutilizable

--- resources/views/administrator/clients.blade.php
+++ resources/views/administrator/clients.blade.php
@@ -54,7 +54,7 @@
                         </div>
                     </div>
                     &nbsp;
-                    <a href="#" class="btn btn-brand btn-elevate btn-icon-sm" data-toggle="modal" data-target="#modal_add">
+                    <a href="#" class="btn btn-brand btn-elevate btn-icon-sm" data-toggle="modal" data-target="#modal_add_client">
                         <i class="la la-plus"></i>
                         Agregar
                     </a>
@@ -70,7 +70,7 @@
                 <thead>
                     <tr>
                         <th class="clean-icon-table">
-               
+
                         </th>
                         <th>Tipo de persona</th>
                         <th>RFC</th>
@@ -93,131 +93,8 @@
 
 
 <!-- start: Modal add  -->
-<div class="modal fade" id="modal_add" tabindex="-1" role="dialog" aria-hidden="true">
-    <div class="modal-dialog modal-lg" role="document">
-        <div class="modal-content">
-            <div class="modal-header">
-                <h5 class="modal-title" id="exampleModalLabel">Agregar</h5>
-                <button type="button" class="close" data-dismiss="modal" aria-label="Close"></button>
-            </div>
-            <form action="#" method="POST" autocomplete="off" role="presentation" enctype="multipart/form-data">
-                @csrf
-                @method('post')
-                <input style="display:none">
-
-                <div class="modal-body">
-                    <div class="row">
-
-                        <div class="col-12">
-
-                            <div class="form-group">
-                                <label for="person_type" class="form-control-label">Tipo de Persona *</label>
-                                <select name="person_type" id="person_type" class="form-control" required>
-                                    <option value="física" {{ old('person_type') == 'física' ? 'selected' : '' }}>Física</option>
-                                    <option value="moral" {{ old('person_type') == 'moral' ? 'selected' : '' }}>Moral</option>
-                                </select>
-                            </div>
-
-
-                            <div class="form-group">
-                                <label for="rfc" class="form-control-label">RFC</label>
-                                <input type="text" name="rfc" class="form-control" maxlength="13" id="rfc" value="{{ old('rfc') }}">
-                            </div>
-
-                            <div class="form-group">
-                                <label for="name" class="form-control-label">Nombre(s) o Razón Social *</label>
-                                <input type="text" name="name" class="form-control" id="name" value="{{ old('name') }}" required>
-                            </div>
-
-                            <div class="form-group" id="last_name_container">
-                                <label for="last_name" class="form-control-label">Primer apellido</label>
-                                <input type="text" name="last_name" class="form-control" id="last_name" value="{{ old('last_name') }}">
-                            </div>
-                            <div class="form-group" id="second_last_name_container">
-                                <label for="second_last_name" class="form-control-label">Segundo apellido</label>
-                                <input type="text" name="second_last_name" class="form-control" id="second_last_name" value="{{ old('second_last_name') }}">
-                            </div>
-
-                            <div class="form-group" id="denomination_container">
-                                <label for="denomination_id" class="form-control-label">Denominación *</label>
-                                <select name="denomination_id" id="denomination_id" class="form-control" required>
-                                    <option value="">Selecciona una denominación</option>
-                                    @foreach($denominations as $denomination)
-                                    <option value="{{ $denomination->id }}" {{ old('denomination_id') == $denomination->id ? 'selected' : '' }}>
-                                        {{ $denomination->acronym }} ({{ $denomination->denomination }})
-                                    </option>
-                                    @endforeach
-                                </select>
-                            </div>
-
-
-
-                            <div class="form-group" id="legal_representative_container">
-                                <label for="legal_representative" class="form-control-label">Representante legal</label>
-                                <input type="text" name="legal_representative" class="form-control" id="legal_representative" value="{{ old('legal_representative') }}">
-                            </div>
-
-                            <div class="form-group">
-                                <label for="phone_number" class="form-control-label">Número Teléfonico </label>
-                                <input type="tel" name="phone_number" class="form-control" id="phone_number" value="{{ old('phone_number') }}" maxlength="10">
-                            </div>
-                            <div class="form-group">
-                                <label for="email" class="form-control-label">Correo Electrónico</label>
-                                <input type="email" name="email" class="form-control" id="email"
-                                    value="{{ old('email') }}">
-                            </div>
-                            <div class="form-group">
-                                <label for="country" class="form-control-label">País</label>
-                                <select name="country" id="country" class="form-control">
-                                    @foreach (Helpers::getCountries() as $key => $value)
-                                    <option value="{{ $value }}">{{ $value }}</option>
-                                    @endforeach
-                                </select>
-                            </div>
-                            <div class="form-group">
-                                <label for="street" class="form-control-label">Calle</label>
-                                <input type="text" name="street" class="form-control" id="street" value="{{ old('street') }}">
-                            </div>
-                            <div class="form-group">
-                                <label for="n_exterior" class="form-control-label">No. Exterior </label>
-                                <input type="text" name="n_exterior" class="form-control" id="n_exterior"
-                                    value="{{ old('n_exterior') }}">
-                            </div>
-                            <div class="form-group">
-                                <label for="suburb" class="form-control-label">Colonia</label>
-                                <input type="text" name="suburb" class="form-control" id="suburb"
-                                    value="{{ old('suburb') }}">
-                            </div>
-                            <div class="form-group">
-                                <label for="municipality" class="form-control-label">Municipio</label>
-                                <input type="text" name="municipality" class="form-control" id="municipality"
-                                    value="{{ old('municipality') }}">
-                            </div>
-                            <div class="form-group">
-                                <label for="entity" class="form-control-label">Entidad</label>
-                                <input type="text" name="entity" class="form-control" id="entity" value="{{ old('entity') }}">
-                            </div>
-                            <div class="form-group">
-                                <label for="zip_code" class="form-control-label">C.P. </label>
-                                <input type="text" name="zip_code" class="form-control" id="zip_code" maxlength="10" value="{{ old('zip_code') }}">
-                            </div>
-                            <div class="form-group">
-                                <label for="observations" class="form-control-label">Observaciones</label>
-                                <textarea name="observations" id="observations" class="form-control" rows="4">{{ old('observations') }}</textarea>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="modal-footer">
-                    <button type="button" class="btn btn-default" data-dismiss="modal">Cancelar</button>
-                    <button type="submit" class="btn btn-primary">Guardar</button>
-                </div>
-            </form>
-        </div>
-    </div>
-</div>
+@include('partials.modal_create_client')
 <!-- end: Modal add  -->
-
 
 
 <!-- start: Modal edit  -->
@@ -371,7 +248,7 @@
                                 @csrf
                                 @method('DELETE')
                                 <input type="hidden" name="id" id="id_delete">
-    
+
                             <div class="modal-body">
                                 <h4 class="text-uppercase text-center">  <i class="flaticon-danger text-danger display-1"></i> <br> ¿Desea realizar esta acción de supresión?</h4>
                             </div>
@@ -406,7 +283,7 @@
 
 
 <script>
-    
+
 
 
     // Numeric only control handler
